9f24bdc39fd27605a8a8b08cb4fe6f23265c80bc
Diego Lira <diego.lira91@gmail.com>
2018-02-11 04:38:57 -0200

    first iteration for seasons

--- PlexMALSync.py
+++ PlexMALSync.py
@@ -77,19 +77,22 @@ def get_plex_watched_shows(lookup_shows):
   logger.info('[PLEX] Retrieving watch count for shows...')
 
   for show in lookup_shows:
-    lookup_show = plex.library.section(plex_anime_section).get(show).episodes()
-    watched_episode_count = 0
+    lookup_show = plex.library.section(plex_anime_section).get(show).episodes()[-1]
+    # watched_episode_count = (episode count, season number)
+    watched_episode_count = (0,1)
     for lookup_result in lookup_show:
         try:
-          if(lookup_result.isWatched and lookup_result.seasonNumber is 1):
+          #if(lookup_result.isWatched and lookup_result.seasonNumber is 1):
+          if (lookup_result.isWatched):
             #logger.debug("%sx%s - watched = %s" % (lookup_result.seasonNumber, lookup_result.index, lookup_result.isWatched))
-            watched_episode_count += 1
+            #watched_episode_count += 1
+            watched_episode_count = (lookup_show.index,lookup_result.seasonNumber)
         except:
           logger.error(Back.RED +'Error during lookup_result processing')
           pass
-    if(watched_episode_count > 0):
+    if(watched_episode_count[0] > 0):
         watched_shows[show] = watched_episode_count
-        #logger.debug('Watched %s episodes for show: %s' % (str(watched_episode_count), show))
+        logger.info('Watched %s episodes for show: %s' % (str(watched_episode_count[0]), show))
 
   logger.info('[PLEX] Retrieving watch count for shows finished')
   return watched_shows
@@ -100,15 +103,76 @@ def get_mal_list():
   item_count = 0
   if(mal_list is not None):
     item_count = len(mal_list)
-
   logger.info('[MAL] Found %s shows on list' % (str(item_count)))
   return mal_list
 
-def send_watched_to_mal(plex_watched_shows, mal_list):
+
+#update_mal_list_with_seasons: instead of one match, let's return all seasons for watched shows and later compare by season.
+# these are seasons defined by MAL. 1 season MIGHT mean a continuous run of many AniDB/TVDB seasons, per MAL standards.
+def update_mal_list_with_seasons(mal_list,plex_watched_shows):
+  logger.info('[MAL] Retrieving updated list for season matching...')
+  mal_list_seasoned = list()
+  for watched_show in plex_watched_shows.keys():
+    mal_shows = spice.search(watched_show,spice.get_medium('anime'),mal_credentials) 
+    matched_list = list()
+    for mal_show in mal_shows:
+      if (mal_show.anime_type == 'TV'):
+        match = (mal_show,mal_show.dates[1] if mal_show.dates[1] != '0000-00-00' else '9999-99-99' )
+        matched_list.append(match)
+    matched_list.sort(key=lambda x: x[1])
+    
+    original_name = [x[0].title for x in matched_list if x[1] != '9999-99-99']
+    #can't do miracles if it's all empty
+    if (not original_name):
+      original_name_treated = matched_list[0][0].title
+    else:
+      original_name_treated = original_name[0]
+    for index,element in enumerate(matched_list):
+      mal_list_seasoned.append((element[0],index+1,original_name_treated))
+  logger.info('[MAL] Retrieving updated list for season matching finished')
+  return mal_list_seasoned
+
+
+#update an existing match
+def update_mal_entry(list_item,plex_title,plex_watched_episode_count,force_update):
+
+  
+  mal_watched_episode_count = int(list_item.episodes)
+  mal_show_id = int(list_item.id)
+  show_in_mal_list = True  
+  if(mal_show_id > 0):
+    if(mal_watched_episode_count < plex_watched_episode_count or force_update):
+      anime_new = spice.get_blank(spice.get_medium('anime'))
+      anime_new.episodes = plex_watched_episode_count
+      new_status = 'watching'
+
+      # if full watched set status to completed, needs additional lookup as total episodes are not exposed in list (mal or spice limitation)
+      lookup_show = spice.search_id(mal_show_id, spice.get_medium('anime'), mal_credentials)
+      if(lookup_show):
+        if(lookup_show.episodes is not None):
+          mal_total_episodes = int(lookup_show.episodes)
+
+          if(plex_watched_episode_count >= mal_total_episodes):
+            new_status = 'completed'
+
+      anime_new.status =  spice.get_status(new_status)
+
+      logger.warn('[PLEX -> MAL] Watch count for %s on Plex is %s and MAL is %s, updating MAL watch count to %s and status to %s ]' % (plex_title, plex_watched_episode_count,
+      mal_watched_episode_count, plex_watched_episode_count, new_status))
+      spice.update(anime_new, mal_show_id, spice.get_medium('anime'), mal_credentials)
+    else:
+      logger.warning( '[PLEX -> MAL] Watch count for %s on Plex was equal or higher on MAL so skipping update' % (plex_title))
+      pass
+
+
+
+def send_watched_to_mal(plex_watched_shows, mal_list, mal_list_seasoned):
   for key, value in plex_watched_shows.items():
     plex_title = key
-    plex_watched_episode_count = value
-    show_in_mal_list = False;
+    plex_watched_episode_count = value[0]
+    plex_watched_episode_season = value[1]
+    show_in_mal_list = False
+    force_update = False
     #logger.debug('%s => watch count = %s' % (plex_title, watched_episode_count))
 
     if(plex_watched_episode_count <= 0 ):
@@ -117,13 +181,23 @@ def send_watched_to_mal(plex_watched_shows, mal_list):
     mal_watched_episode_count = 0
     mal_show_id = 0
 
+    # all shows with season > 1 were previously searched and are part of the mal_list_seasoned object
+    if (plex_watched_episode_season > 1):
+      force_update = True
+      for anime,season,original_name in mal_list_seasoned:
+        if(original_name.lower() == plex_title.lower()):
+            correct_item = [value[0] for index, value in enumerate(mal_list_seasoned) if value[1] == plex_watched_episode_season and value[2] == original_name][0]
+            update_mal_entry(correct_item,plex_title,plex_watched_episode_count,force_update)
+            break
+      continue
+
+
     # check if show is already on MAL list
     for list_item in mal_list:
       #logger.debug('Comparing %s with %s' % (list_item.title, plex_title))
       mal_id =int(list_item.id)
       mal_title = list_item.title
       mal_title_english = ""
-
       if(list_item.english is not None):
         mal_title_english = list_item.english
         #logger.debug('Comparing original: %s | english: %s with %s' % (mal_title, mal_title_english, plex_title))
@@ -133,33 +207,7 @@ def send_watched_to_mal(plex_watched_shows, mal_list):
 
       if(mal_title.lower() == plex_title.lower() or mal_title_english.lower() == plex_title.lower()):
         #logger.debug('%s [%s] was already in list => status = %s | watch count = %s' % (plex_title, list_item.id, spice.get_status(list_item.status), list_item.episodes))
-        mal_watched_episode_count = int(list_item.episodes)
-        mal_show_id = int(list_item.id)
-        show_in_mal_list = True
-         
-        if(mal_watched_episode_count > 0 and mal_show_id > 0):
-          if(mal_watched_episode_count < plex_watched_episode_count):
-            anime_new = spice.get_blank(spice.get_medium('anime'))
-            anime_new.episodes = plex_watched_episode_count
-            new_status = 'watching'
-
-            # if full watched set status to completed, needs additional lookup as total episodes are not exposed in list (mal or spice limitation)
-            lookup_show = spice.search_id(mal_id, spice.get_medium('anime'), mal_credentials)
-            if(lookup_show):
-              if(lookup_show.episodes is not None):
-                mal_total_episodes = int(lookup_show.episodes)
-
-                if(plex_watched_episode_count >= mal_total_episodes):
-                  new_status = 'completed'
-
-            anime_new.status =  spice.get_status(new_status)
-
-            logger.warn('[PLEX -> MAL] Watch count for %s on Plex is %s and MAL is %s, updating MAL watch count to %s and status to %s ]' % (plex_title, plex_watched_episode_count,
-            mal_watched_episode_count, plex_watched_episode_count, new_status))
-            spice.update(anime_new, mal_show_id, spice.get_medium('anime'), mal_credentials)
-          else:
-            logger.warning( '[PLEX -> MAL] Watch count for %s on Plex was equal or higher on MAL so skipping update' % (plex_title))
-            pass
+        update_mal_entry(list_item,plex_title,plex_watched_episode_count,force_update)
 
     # if not listed in list lookup on MAL
     if(not show_in_mal_list):
@@ -191,25 +239,24 @@ def send_watched_to_mal(plex_watched_shows, mal_list):
             mal_list_watched_episode_count = int(list_item.episodes)
 
             if(mal_list_id == mal_show_id):
-              on_mal_list = True;
+              on_mal_list = True
               if(plex_watched_episode_count == mal_list_watched_episode_count):
                 logger.warning('[PLEX -> MAL] show was found in current MAL list using id lookup however watch count was identical so skipping update')
                 update_list = False
               break
 
           if(update_list):
+            logger.warn('[PLEX -> MAL] Found match on MAL and setting state to watching with watch count: %s' % (plex_watched_episode_count))
             anime_new = spice.get_blank(spice.get_medium('anime'))
             anime_new.episodes = plex_watched_episode_count
 
             if(plex_watched_episode_count >= mal_total_episodes):
-                logger.warn('[PLEX -> MAL] Found match on MAL and setting state to completed as watch count equal or higher than total episodes')
                 anime_new.status = spice.get_status('completed')
                 if(on_mal_list):
                   spice.update(anime_new, mal_show.id, spice.get_medium('anime'), mal_credentials)
                 else:
                   spice.add(anime_new, mal_show.id, spice.get_medium('anime'), mal_credentials)
             else:
-                logger.warn('[PLEX -> MAL] Found match on MAL and setting state to watching with watch count: %s' % (plex_watched_episode_count))
                 anime_new.status = spice.get_status('watching')
                 if(on_mal_list):
                   spice.update(anime_new, mal_show.id, spice.get_medium('anime'), mal_credentials)
@@ -233,7 +280,9 @@ def start():
   plex_watched_shows = get_plex_watched_shows(plex_shows)
 
   # finally compare lists and update MAL where needed
-  send_watched_to_mal(plex_watched_shows, mal_list)
+  updated_mal_list = update_mal_list_with_seasons(mal_list,plex_watched_shows)
+
+  send_watched_to_mal(plex_watched_shows, mal_list, updated_mal_list)
 
   logger.info('Plex to MAL sync finished')
 
